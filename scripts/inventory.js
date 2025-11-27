bd77c2cab88e7123bd151223187a4ce61e17e062
Kosmosaik <alexander.renstrom@gmail.com>
2025-11-27 07:58:54 +0100

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -5,17 +5,3 @@ const inventoryList = document.getElementById("inventory-list");
 
 // Inventory data
 const inventory = Object.create(null);
-
-// Category helpers
-function categoryHeaderLabel(category = "Other") {
-  const map = {
-    "Material": "MATERIALS",
-    "Crafting Component": "CRAFTING COMPONENTS",
-    "Resource": "RESOURCES",
-    "Weapon": "WEAPONS",
-    "Tool": "TOOLS",
-    "Wood": "WOOD",
-    "Food": "FOOD",
-  };
-  return map[category] || (category || "OTHER").toUpperCase();
-}
