cfe890230a3bb5c4cd7a43288b81f4b58e0c4e69
Kosmosaik <alexander.renstrom@gmail.com>
2025-12-01 15:07:21 +0100

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 // scripts/config.js
 // Central place for tweakable settings.
 const GAME_CONFIG = {
-  version: "0.0.67-dev",
+  version: "0.0.67-stable",
 
   loot: {
     // Progress bar behavior
