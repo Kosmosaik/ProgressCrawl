c88a03165016f8c33b73d3b4d412eaca912adb0d
Kosmosaik <alexander.renstrom@gmail.com>
2025-12-17 12:11:01 +0100

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -4,6 +4,7 @@
 const ItemCatalog = [
 // ========== WEAPONS ==========
   {
+    id: "wpn_crude_knife",
     name: "Crude Knife",
     description: "A primitive cutting tool made from stone and twine.",
     category: "Weapon",
@@ -13,12 +14,13 @@ const ItemCatalog = [
     skillReq: 3,
     attrPerPower: 1.5,
     statRanges: {
-    damage: [2, 4],
-    attackSpeed: [0.9, 1.1],
-    critChance: [0, 1],
+      damage: [2, 4],
+      attackSpeed: [0.9, 1.1],
+      critChance: [0, 1],
     }
   },
   {
+    id: "wpn_wooden_club",
     name: "Wooden Club",
     description: "A branch trimmed into a club",
     category: "Weapon",
@@ -28,11 +30,12 @@ const ItemCatalog = [
     skillReq: 3,
     attrPerPower: 1.5,
     statRanges: {
-    damage: [3, 6],
-    attackSpeed: [0.8, 1.0],
+      damage: [3, 6],
+      attackSpeed: [0.8, 1.0],
     }
   },
   {
+    id: "wpn_stone_knife",
     name: "Stone Knife",
     description: "A simple sharpened knife, nothing fancy.",
     category: "Weapon",
@@ -42,12 +45,13 @@ const ItemCatalog = [
     skillReq: 10,
     attrPerPower: 1.6,
     statRanges: {
-    damage: [4, 8],
-    attackSpeed: [1.0, 1.2],
-    critChance: [1, 2],
+      damage: [4, 8],
+      attackSpeed: [1.0, 1.2],
+      critChance: [1, 2],
     }
   },
   {
+    id: "wpn_stone_headed_club",
     name: "Stone-Headed Club",
     description: "Heavier and stronger than Wooden Club",
     category: "Weapon",
@@ -57,11 +61,12 @@ const ItemCatalog = [
     skillReq: 3,
     attrPerPower: 1.5,
     statRanges: {
-    damage: [5, 9],
-    attackSpeed: [0.75, 0.95],
+      damage: [5, 9],
+      attackSpeed: [0.75, 0.95],
     }
   },
   {
+    id: "wpn_stone_axe",
     name: "Stone Axe",
     description: "Basic Stone Axe",
     category: "Weapon",
@@ -71,11 +76,12 @@ const ItemCatalog = [
     skillReq: 3,
     attrPerPower: 1.5,
     statRanges: {
-    damage: [4, 10],
-    attackSpeed: [0.75, 0.95],
+      damage: [4, 10],
+      attackSpeed: [0.75, 0.95],
     }
   },
   {
+    id: "wpn_leet_power_sword_of_doom",
     name: "Leet Power Sword of Doom",
     description: "Boi you found it!",
     category: "Weapon",
@@ -85,14 +91,16 @@ const ItemCatalog = [
     skillReq: 100,
     attrPerPower: 2,
     statRanges: {
-    damage: [120, 200],
-    attackSpeed: [1.1, 1.4],
-    critChance: [5, 20],
-    lootFind: [0, 10],
+      damage: [120, 200],
+      attackSpeed: [1.1, 1.4],
+      critChance: [5, 20],
+      lootFind: [0, 10],
     }
   },
+
 // ========== ARMOR / EQUIPMENT ==========
   {
+    id: "arm_bark_chest",
     name: "Bark Chest",
     description: "Layers of bark binded together with twine for rudimentary protection",
     category: "Armor",
@@ -100,10 +108,11 @@ const ItemCatalog = [
     rarity: "Uncommon",
     usage: "Basic armor.",
     statRanges: {
-    vit: [1, 4],
+      vit: [1, 4],
     }
   },
   {
+    id: "arm_grass_wraps_chest",
     name: "Grass Wraps Chest",
     description: "Primitive chest clothing. Minimal protection",
     category: "Armor",
@@ -111,10 +120,11 @@ const ItemCatalog = [
     rarity: "Uncommon",
     usage: "Basic armor.",
     statRanges: {
-    vit: [1, 2],
+      vit: [1, 2],
     }
   },
   {
+    id: "arm_grass_wraps_loincloth",
     name: "Grass Wraps Loincloth",
     description: "Primitive clothing. Minimal protection",
     category: "Armor",
@@ -122,11 +132,13 @@ const ItemCatalog = [
     rarity: "Uncommon",
     usage: "Basic armor.",
     statRanges: {
-    vit: [1, 2],
+      vit: [1, 2],
     }
   },
+
 // ========== MATERIALS ==========
   {
+    id: "mat_mythic_fragment",
     name: "Mythic Fragment",
     category: "Material",
     description: "Super rare thingy McDingy",
@@ -134,6 +146,7 @@ const ItemCatalog = [
     usage: "eh oh",
   },
   {
+    id: "mat_healing_herb",
     name: "Healing Herb",
     category: "Material",
     description: "A fragrant herb used to create basic healing potions.",
@@ -141,14 +154,17 @@ const ItemCatalog = [
     usage: "Restores 10 HP when brewed or eaten raw.",
   },
   {
+    id: "mat_rope",
     name: "Rope",
     category: "Material",
     description: "A length of rope used for traps, crafting, and bindings.",
     rarity: "Common",
     usage: "Crafting, traps",
   },
-  // ========== TOOLS ==========
+
+// ========== TOOLS ==========
   {
+    id: "tool_basic_fishing_rod",
     name: "Basic Fishing Rod",
     category: "Tool",
     description: "A basic fishing rod used to fish.",
@@ -156,6 +172,7 @@ const ItemCatalog = [
     usage: "Fishing",
   },
   {
+    id: "tool_primitive_pickaxe",
     name: "Primitive Pickaxe",
     category: "Tool",
     description: "A basic pickaxe used to mine basic ores",
@@ -163,6 +180,7 @@ const ItemCatalog = [
     usage: "Mining",
   },
   {
+    id: "tool_digging_stick",
     name: "Digging Stick",
     category: "Tool",
     description: "A basic tool for digging up roots and tubers",
@@ -170,14 +188,17 @@ const ItemCatalog = [
     usage: "Digging",
   },
   {
+    id: "tool_cage_trap",
     name: "Cage Trap",
     category: "Tool",
     description: "To catch small animals.",
     rarity: "Rare",
     usage: "Digging",
   },
-  // ========== RESOURCES ==========
+
+// ========== RESOURCES ==========
   {
+    id: "res_animal_hide",
     name: "Animal Hide",
     category: "Resource",
     description: "Animal hide used for leatherworking and basic armor.",
@@ -185,6 +206,7 @@ const ItemCatalog = [
     usage: "Crafting, armor",
   },
   {
+    id: "res_contaminated_water",
     name: "Contaminated Water",
     category: "Resource",
     description: "Untreated water. Drinking is not recommended.",
@@ -192,6 +214,7 @@ const ItemCatalog = [
     usage: "Ropes, simple baskets, fire tinder",
   },
   {
+    id: "res_oak_log",
     name: "Oak Log",
     category: "Resource",
     description: "Wood material used for crafting and building.",
@@ -199,6 +222,7 @@ const ItemCatalog = [
     usage: "Crafting, building, fuel",
   },
   {
+    id: "res_purified_water",
     name: "Purified Water",
     category: "Resource",
     description: "Treated water. Drinkable and can be used in cooking.",
@@ -206,6 +230,7 @@ const ItemCatalog = [
     usage: "Ropes, simple baskets, fire tinder",
   },
   {
+    id: "res_grass_bundle",
     name: "Grass Bundle",
     category: "Resource",
     description: "A basic, common plant material. Used for crafting simple items and building basic structures.",
@@ -213,6 +238,7 @@ const ItemCatalog = [
     usage: "Ropes, simple baskets, fire tinder",
   },
   {
+    id: "res_plant_fibre",
     name: "Plant Fibre",
     category: "Resource",
     description: "Plant fibers gathered from fibrous plants, like hemp, flax, or wild grasses",
@@ -220,6 +246,7 @@ const ItemCatalog = [
     usage: "Ropes, simple baskets, fire tinder",
   },
   {
+    id: "res_dry_leaves",
     name: "Dry Leaves",
     category: "Resource",
     description: "Good as kindling or for thatched roof",
@@ -227,6 +254,7 @@ const ItemCatalog = [
     usage: "eh",
   },
   {
+    id: "res_twig",
     name: "Twig",
     category: "Resource",
     description: "A small slender branch fallen from a tree or a bush.",
@@ -234,6 +262,7 @@ const ItemCatalog = [
     usage: "Ropes, simple baskets, fire tinder",
   },
   {
+    id: "res_small_stone",
     name: "Small Stone",
     category: "Resource",
     description: "Basic stone material used for crafting and building.",
@@ -241,6 +270,7 @@ const ItemCatalog = [
     usage: "Craft primitive items",
   },
   {
+    id: "res_flint",
     name: "Flint",
     category: "Resource",
     description: "A hard stone used for primitive tools and fire-starting.",
@@ -248,6 +278,7 @@ const ItemCatalog = [
     usage: "Crafting, fire-starting",
   },
   {
+    id: "res_sap",
     name: "Sap",
     category: "Resource",
     description: "Sticky tree sap used in primitive crafting and adhesives.",
@@ -255,6 +286,7 @@ const ItemCatalog = [
     usage: "Crafting, adhesive",
   },
   {
+    id: "res_wooden_branch",
     name: "Wooden Branch",
     category: "Resource",
     description: "Wooden branch used in primitive crafting, campfires etc.",
@@ -262,6 +294,7 @@ const ItemCatalog = [
     usage: "Craft primitive items",
   },
   {
+    id: "res_birch_wood",
     name: "Birch Wood",
     category: "Resource",
     description: "A wood type used in crafting, carpentry etc",
@@ -269,6 +302,7 @@ const ItemCatalog = [
     usage: "Make planks, carpentry",
   },
   {
+    id: "res_iron_ore",
     name: "Iron Ore",
     category: "Resource",
     description: "A chunk of iron ore ready to be smelted.",
@@ -276,14 +310,17 @@ const ItemCatalog = [
     usage: "Refine into ingots at a forge.",
   },
   {
+    id: "res_copper_nuggets",
     name: "Copper Nuggets",
     category: "Resource",
     description: "Small pieces of copper, useful in crafting basic metal items and refining.",
     rarity: "Common",
     usage: "Refine into ingots at a forge.",
   },
+
 // ========== FOOD ==========
   {
+    id: "food_raw_squirrel_meat",
     name: "Raw Squirrel Meat",
     category: "Food",
     description: "Poor thing.",
@@ -291,6 +328,7 @@ const ItemCatalog = [
     usage: "Eh",
   },
   {
+    id: "food_raw_wolf_meat",
     name: "Raw Wolf Meat",
     category: "Food",
     description: "Raw Meat. Can be cooked for better effects.",
@@ -298,14 +336,17 @@ const ItemCatalog = [
     usage: "Food, cooking",
   },
   {
+    id: "food_raw_rabbit_meat",
     name: "Raw Rabbit Meat",
     category: "Food",
     description: "Raw Meat. Can be cooked for better effects.",
     rarity: "Common",
     usage: "Food, cooking",
   },
+
 // ========== CONSUMABLES ==========
   {
+    id: "con_bandage",
     name: "Bandage",
     category: "Consumable",
     description: "A basic bandage used to treat wounds.",
